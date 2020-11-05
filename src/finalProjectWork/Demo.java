9a3ff9a3467334790d952f21f9e08b66610043ec
Eugen02 <zhenyazhogolev70@gmail.com>
2020-11-05 21:28:01 +0200

    finalProj

--- src/finalProjectWork/Demo.java
+++ src/finalProjectWork/Demo.java
@@ -1,12 +1,38 @@
 package finalProjectWork;
 
+import java.util.Iterator;
+
 public class Demo {
     public static void main(String[] args) {
-
-
+        int i = 0;
      Matrix matrix = new Matrix(5);
      matrix.print();
+     matrix.getColumnRow(1,1);
+     matrix.getColumnRow(4,4);
+     matrix.setColumnRow(4,4,100);
+     matrix.getColumnRow(4,4);
+     matrix.matrixSize();
+     matrix.print();
 
+    matrix.notEmptyElement();
+    matrix.notEmptyElementColumn(1);
+    matrix.notEmptyElementRow(2);
+    matrix.notEmptyElementRow(4);
+    matrix.emptyElementRow(4);
+    matrix.emptyElement();
+
+
+        Iterator<Object> iter = matrix.iterator();
+        while(iter.hasNext()){
+            if (i == matrix.getSize()){
+                System.out.println();
+                i=0;
+            }
+            System.out.printf("%8s",iter.next());
+            i++;
+        }
+
+        System.out.println(matrix.toString());
 
 
     }
--- src/finalProjectWork/Matrix.java
+++ src/finalProjectWork/Matrix.java
@@ -1,13 +1,11 @@
 package finalProjectWork;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 
 public class Matrix {
     private Map<Integer,Integer[]> arrays = new HashMap<>();
-    int size;
+    private int size;
     public Matrix(){ }
 
     public Matrix(int size) {
@@ -26,10 +24,13 @@ public class Matrix {
         }
     }
 
-    public void setArray(){
-
+    public Map<Integer, Integer[]> getArrays() {
+        return arrays;
     }
 
+    public int getSize() {
+        return size;
+    }
 
     public void print(){
         StringBuilder sb = new StringBuilder();
@@ -49,15 +50,15 @@ public class Matrix {
 
     public void getColumnRow(int column, int row){
         Integer[] array = arrays.get(column);
-        if (array[row] == 0){
+        if (array[row] == null){
             System.out.println("null");
         }
-        else System.out.println(array[row]);
+        else System.out.println("Значение в этой ячейке = " +array[row]);
     }
 
     public void setColumnRow(int column, int row, int value){
         Integer[] array = arrays.get(column);
-        if (array[row] == 0){
+        if (array[row] == null){
             try {
                 throw new MatrixException();
             } catch (MatrixException e) {
@@ -80,6 +81,9 @@ public class Matrix {
         for (int i = 0; i<size; i++){
             array = arrays.get(i);
             for (int j = 0; j < size; j++){
+                if (array[j] == null){
+                    continue;
+                }
                 if (array[j] != 0){
                     notEmpty++;
                 }
@@ -94,6 +98,9 @@ public class Matrix {
         for (int i = 0; i<size; i++){
             array = arrays.get(i);
             for (int j = 0; j < size; j++){
+                if (array[j] == null){
+                    continue;
+                }
                 if (array[j] != 0 && j==column){
                     template++;
                 }
@@ -101,15 +108,130 @@ public class Matrix {
         }
         System.out.println("Not empty elements in column = " + template);
     }
-    public void notEmptyElementRow(int row){
+    public void notEmptyElementRow(int row) {
         int template = 0;
         Integer[] array = arrays.get(row);
-        for (int i = 0;i<size;i++){
-            if (array[i]!=0){
+        for (int i = 0; i < size; i++) {
+            if (array[i] == null) {
+                continue;
+            }
+            if (array[i] != 0) {
                 template++;
             }
         }
         System.out.println("Not empty elements in row = " + template);
+    }
+
+    public void emptyElement(){
+            int Empty = 0;
+            Integer[] array;
+            for (int i = 0; i<size; i++){
+                array = arrays.get(i);
+                for (int j = 0; j < size; j++){
+                    if (array[j] == null){
+                        Empty++;
+                        continue;
+                    }
+                    if (array[j] == 0){
+                        Empty++;
+                    }
+                }
+            }
+        System.out.println("Empty elements (null too) = " + Empty);
+    }
+
+    public void emptyElementRow(int row) {
+        int template = 0;
+        Integer[] array = arrays.get(row);
+        for (int i = 0; i < size; i++) {
+            if (array[i] == null) {
+                continue;
+            }
+            if (array[i] == 0) {
+                template++;
+            }
+        }
+        System.out.println("Empty elements in row = " + template);
+    }
+
+
+    public void emptyElementColumn(int column){
+        int template = 0;
+        Integer[] array;
+        for (int i = 0; i<size; i++){
+            array = arrays.get(i);
+            for (int j = 0; j < size; j++){
+                if (array[j] == null){
+                    continue;
+                }
+                if (array[j] == 0 && j==column){
+                    template++;
+                }
+            }
+        }
+        System.out.println("Empty elements in column = " + template);
+    }
+
+
+    public Iterator<Object> iterator() {
+        return new IteratorMatrix();
+    }
+
+    private class IteratorMatrix implements Iterator<Object> {
+       int size = getSize();
+       int cursor1;
+       int cursor2;
+       int lastRet = -1;
+        Map<Integer,Integer[]> arrays = getArrays();
+        Integer[] array;
+
+        @Override
+        public boolean hasNext()
+        {
+            return cursor1 !=size;
+        }
+
+        @Override
+        public Object next()
+        {
+            if(!hasNext()){throw new NoSuchElementException();}
+            if (cursor2 == size){
+                cursor1++;
+                cursor2 = 0;
+            }
+            int i = cursor2;
+            cursor2 = i + 1;
+            lastRet = i;
+            array = arrays.get(cursor1);
+            if (lastRet == 4 && cursor1 == 4){
+                cursor1 = size;
+                return  array[lastRet];
+            }
+            return   array[lastRet];
+        }
+    }
+
+    @Override
+    public String toString() {
+        Iterator<Object> objectIterator = iterator();
+        int template = 0;
+        if (! objectIterator.hasNext())
+            return "[]";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.lineSeparator()).append('[');
+        for (;;) {
+            Object e = objectIterator.next();
+            sb.append(e == this ? "(this Collection)" : e);
+            if (! objectIterator.hasNext())
+                return sb.append(']').toString();
+            if (template == size-1){
+                sb.append("]").append(System.lineSeparator());
+                template = 0;
+                sb.append("[");
+            }else {
+            template++;
+            sb.append(", ");}}
     }
 
 
